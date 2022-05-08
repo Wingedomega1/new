81fe4024cdef9ba73efebf15b4ac329a11301c53
unknown <nevilleblast2238@gmail.com>
2022-05-08 14:15:32 +0530

    final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const matchList = document.getElementById('match-list');
 //search the states.json and filter it 
 const searchStates= async searchText =>{
 
-    const res = await fetch('state.json');
+    const res = await fetch('in.json');
     const states=  await res.json();
 
     console.log(states);
@@ -14,7 +14,7 @@ const searchStates= async searchText =>{
 
 let matches = states.filter(state=>{
     const regex = new RegExp(`^${searchText}`,'gi');
-    return state.name.match(regex) || state.abbr.match(regex);
+    return state.city.match(regex) || state.admin_name.match(regex);
 });
 
 
@@ -42,9 +42,9 @@ const outputHtml= matches =>{
         const html  = matches.map(match =>
             `
             <div class="card card-body mb-1">
-            <h4> ${match.name} (${match.abbr}) <span class=" text-primary">
-            ${match.capital}</span></h4>
-            <small> Lat: ${match.lat}/ Long: ${match.long}</small>
+            <h4> ${match.city} (${match.admin_name} , <span class=" text-primary">
+            ${match.capital}</span>)</h4>
+            <small> Lat: ${match.lat}/ Long: ${match.lng}</small>
             </div>
             `
             ).join('');
